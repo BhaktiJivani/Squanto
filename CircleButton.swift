af2072f28dd3412ab4bb2c89244098d2cb492178
BhaktiJivani <jivanibhakti@gmail.com>
2017-02-08 09:49:45 -0800

    first commit

---
+++ CircleButton.swift
@@ -0,0 +1,21 @@
+//
+//  CircleButton.swift
+//  Squanto
+//
+//  Created by 16246 on 12/1/16.
+//  Copyright © 2016 SBhimani.SquantoTeam. All rights reserved.
+//
+
+import UIKit
+
+class CircleButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
